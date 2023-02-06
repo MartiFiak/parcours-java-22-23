f012376ef7eade9ba749e9c6412e5376c4b9aab6
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 10:51:18 +0100

    concat

--- StringConcat.java
+++ StringConcat.java
@@ -1,10 +1,10 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         if (s1 == null) {
-            return "";
+            return s2;
         }
         if (s2 == null) {
-            return "";
+            return s1;
         }
          String concat = s1 + s2;
          return concat;
