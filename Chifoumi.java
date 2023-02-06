f74f15aba5cfc89e18f24561b7dd2e63f3064af0
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 10:32:34 +0100

    Chifoumi

---
+++ Chifoumi.java
@@ -0,0 +1,18 @@
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        switch(chifoumiAction) {
+
+            case ROCK:
+            return ChifoumiAction.PAPER;
+
+            case PAPER:
+            return ChifoumiAction.SCISSOR;
+
+            case SCISSOR:
+            return ChifoumiAction.ROCK;
+
+            default:
+            return null;
+        }
+    }
+}
