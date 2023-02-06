1ebb2ca47dbfd72fd1cad86ba9487202bd33cd97
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 10:07:30 +0100

    IntOperationJava

---
+++ IntOperation.java
@@ -0,0 +1,18 @@
+public class IntOperation {
+    public static int addTwoIntegers(int a, int b) {
+        int add = a + b ;
+        return add;
+    }
+    public static int subtractTwoIntegers(int a, int b) {
+        int sub = a - b ;
+        return sub;
+    }
+    public static int multiplyTwoIntegers(int a, int b) {
+        int mult= a * b ;
+        return mult;
+    }
+    public static int divideTwoIntegers(int a, int b) {
+        int div = a / b;
+        return div;
+    }
+}
