ef32f4dc4c5061a9089314c3ee74e36ead97af6e
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 15:23:25 +0100

    list

--- ListSearchIndex.java
+++ ListSearchIndex.java
@@ -15,12 +15,16 @@ public class ListSearchIndex {
         return list.indexOf(value);
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        if (value == null || list == null) {
+            return new ArrayList<Integer>();
+        }
+        
         List<Integer> newlist = new ArrayList<Integer>();
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(value)) {
                 newlist.add(i);
             }
         }
-        return newlist;
+        return newlist
     }
 }
