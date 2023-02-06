54d19688516084c188d4f4c61ba2cac2d8e6ea82
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 15:45:24 +0100

    listequal

---
+++ ListEqual.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ListEqual {
+    public static boolean areListEquals(List<String> list1, List<String> list2) {
+        if (list1.equals(list2)) {
+            return true;
+        }
+        return false;
+    }
+}
