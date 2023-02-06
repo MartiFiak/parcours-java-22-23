87d5625bd6fedd4b6215d4859358a8f63c99855f
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 15:19:22 +0100

    list

---
+++ ListSearchIndex.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSearchIndex {
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (value == null || list == null) {
+            return null;
+        }
+        return list.lastIndexOf(value);
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (value == null || list == null) {
+            return null;
+        }
+        return list.indexOf(value);
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> newlist = new ArrayList<Integer>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                newlist.add(i);
+            }
+        }
+        return newlist;
+    }
+}
