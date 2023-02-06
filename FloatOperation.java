a0dacbce2e74a2019e2c43b2d95e73bca3415cda
MartiFiak <jacolotmarine@gmail.com>
2023-02-06 10:09:59 +0100

    ExerciseRunner

---
+++ FloatOperation.java
@@ -0,0 +1,10 @@
+public class FloatOperation {
+    public static float addTwoFloats(float a, float b) {
+        float add = a + b;
+        return add;
+    }
+    public static float divideTwoFloats(float a, float b) {
+        float div = a / b;
+        return div;
+    }
+}
