4483b1447a86ed760ddc280a3be4c5bfdf05aca8
MartiFiak <jacolotmarine@gmail.com>
2023-02-09 15:03:22 +0100

    celest

---
+++ CelestialObject.java
@@ -0,0 +1,10 @@
+public class CelestialObject {
+    
+    public static double x = 0.0;      
+
+    public static double y = 0.0;
+
+    public static double z = 0.0;
+
+    public static String name = null;
+}
