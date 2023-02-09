3e804e67902fdbecdec0e6610124adc62a18ac94
MartiFiak <jacolotmarine@gmail.com>
2023-02-09 15:56:45 +0100

    celest

--- CelestialObject.java
+++ CelestialObject.java
@@ -15,4 +15,36 @@ public class CelestialObject {
         this.y = y;
         this.z = z;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
