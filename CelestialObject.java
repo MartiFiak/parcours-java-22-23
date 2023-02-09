b2d843808dc8fbc5a8345d17c38bd17634e9e044
MartiFiak <jacolotmarine@gmail.com>
2023-02-09 15:53:14 +0100

    celest

--- CelestialObject.java
+++ CelestialObject.java
@@ -1,42 +1,18 @@
 public class CelestialObject {
     
     public double x = 0.0;      
-
     public double y = 0.0;
-
     public double z = 0.0;
-
     public String name = null;
 
-    public double getX() {
-        return x;
+    public CelestialObject() {
+        this("Soleil", 0.0, 0.0, 0.0);
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getZ() {
-        return z;
-    }
-
-    public void setZ(double z) {
-        this.z = z;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public CelestialObject(String name, double x, double y, double z) {
         this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 }
