ccb009d73b7fdbe113d9054f1c3a958f2d39982e
MartiFiak <jacolotmarine@gmail.com>
2023-02-09 11:41:09 +0100

    change

---
+++ Wedding.java
@@ -0,0 +1,34 @@
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Wedding {
+    public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
+        if (first == null || second == null) {
+            return null;
+        }
+        List<String> lfirst = new ArrayList<>(first);
+        List<String> lsecond = new ArrayList<>(second);
+        Map<String, String> map = new HashMap<>();
+        if(lfirst.size() == lsecond.size()) {
+            for (int i = 0; i < lfirst.size(); i++) {
+                map.put(lfirst.get(i), lsecond.get(i));
+            }
+        }
+        if(lfirst.size() < lsecond.size()) {
+            for (int i = 0; i < lfirst.size(); i++) {
+                map.put(lfirst.get(i), lsecond.get(i));
+            }
+        }
+        if(lfirst.size() == lsecond.size()) {
+            for (int i = 0; i < lfirst.size(); i++) {
+                map.put(lfirst.get(i), lsecond.get(i));
+            }
+        }
+        return map;
+
+    }
+    
+}
