51acbfc61f42beb89f9284b94dfdabf8caef1647
MartiFiak <jacolotmarine@gmail.com>
2023-02-10 09:37:27 +0100

    star

--- Star.java
+++ Star.java
@@ -1,3 +1,11 @@
-public class Star {
-    
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
 }
