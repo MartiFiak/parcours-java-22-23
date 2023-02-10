65e856596e89608bd37f689118c803afa88afd4c
MartiFiak <jacolotmarine@gmail.com>
2023-02-10 16:56:16 +0100

    lol

--- Character.java
+++ Character.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
 
     private final int maxHealth;
     private int currentHealth;
     private final String name;
+    private static List <Character>allCharacters = new ArrayList<>();
 
 
     public int getMaxHealth() {
@@ -33,9 +37,51 @@ public class Character {
         this.maxHealth = maxHealth;
         this.name = name;
         this.currentHealth = maxHealth;
+        this.allCharacters.add(this);
     }
 
 
+    public static String printStatus() {
+        String status ="------------------------------------------\n" +
+        "Characters currently fighting : \n";
+        
+        if (allCharacters.size() == 0) {
+            status = "------------------------------------------\n" +
+                    "Nobody's fighting right now !\n" +
+                    "------------------------------------------\n";
+        } else {
+            
+            for (Character character : allCharacters) {
+                status += " - " + character.toString() + "\n";
+            }
+            status +=  "------------------------------------------";
+        }
+        
+        return status;
+    }
+
+
+    public static Character fight(Character player1, Character player2) {
+        Character winner;
+        int round = 1;
+        while (player1.getCurrentHealth() > 0 && player2.getCurrentHealth() > 0) {
+            if (round == 1){
+            player1.attack(player2);
+            round = 2;
+            } else {
+                player2.attack(player1);
+                round = 1;
+            }
+        }
+        if (player1.getCurrentHealth() > 0) {
+            winner = player1;
+        } else {
+            winner = player2;
+        }
+        return winner;
+    }
+
+    
     @Override
     public String toString() {
         if (currentHealth <= 0) {
