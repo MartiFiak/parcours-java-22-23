968d5c3830271acf96ba6ad230632f1570a51fcb
MartiFiak <jacolotmarine@gmail.com>
2023-02-10 10:01:41 +0100

    star

--- Star.java
+++ Star.java
@@ -1,4 +1,32 @@
 public class Star extends CelestialObject {
+    @Override
+    public String toString() {
+        return getName() + " shines at the " + String.format("%.3f", getMagnitude()) + " magnitude";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(magnitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Star other = (Star) obj;
+        if (Double.doubleToLongBits(magnitude) != Double.doubleToLongBits(other.magnitude))
+            return false;
+        return true;
+    }
     private double magnitude;
 
     public void setMagnitude(double magnitude) {
@@ -7,5 +35,12 @@ public class Star extends CelestialObject {
 
     public double getMagnitude() {
         return magnitude;
+    }
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    public Star() {
     }
 }
