0af02ba143ab427c39dc63cc5ce40d464a6f5303
MartiFiak <jacolotmarine@gmail.com>
2023-02-10 11:52:29 +0100

    adventure

---
+++ Character.java
@@ -0,0 +1,42 @@
+public class Character {
+
+    private int maxHealth;
+    public int currentHealth;
+    private String name;
+
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    public void takeDamage(int i) {
+        currentHealth -= i;
+    }
+    public void attack(Character uruk) {
+        uruk.takeDamage(9);
+    }
+
+
+    public Character(String name, int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.name = name;
+        this.currentHealth = maxHealth;
+    }
+
+
+    @Override
+    public String toString() {
+        if (currentHealth <= 0) {
+            return getName() + " : KO";
+        } else {
+        return getName() + " : " + getCurrentHealth() + "/" + getMaxHealth();
+        }
+    }
+}
--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,21 +1,18 @@
-import java.util.List;
-
 public class ExerciseRunner {
 
     public static void main(String[] args) {
-        Galaxy galaxy = new Galaxy();
-        CelestialObject lune = new CelestialObject("Lune", -123.12, 392.238, 32.31);
-        Star betelgeuse = new Star("Betelgeuse", 128.23, -12.82, 32.328, 1289.3);
-        Planet naboo = new Planet("Naboo", 17.4389, 8349.1, 8943.92, betelgeuse);
+        Character aragorn = new Character("Aragorn", 20);
+        Character uruk = new Character("Uruk", 5);
         
-        galaxy.addCelestialObject(lune);
-        galaxy.addCelestialObject(betelgeuse);
-        galaxy.addCelestialObject(naboo);
+        System.out.println(aragorn.toString());
+        System.out.println(uruk.toString());
         
-        List<CelestialObject> celestialObjects = galaxy.getCelestialObjects();
+        aragorn.attack(uruk);
+
+        System.out.println(uruk.toString());
         
-        for (CelestialObject celestialObject : celestialObjects) {
-            System.out.println(celestialObject.toString());
-        }
+        aragorn.takeDamage(12);
+
+        System.out.println(aragorn.toString());
     }
 }
